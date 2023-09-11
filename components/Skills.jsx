62705f08dae17ff0745602eb3c0b041518a70e75
Emilijus <emilijus.sileikis@mif.stud.vu.lt>
2023-09-11 16:48:07 +0300

    Updates

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -4,8 +4,8 @@ import SkillList from "./SkillList";
 
 function Skills() {
     return (
-        <section id="skills" className={`flex flex-wrap text-white ${styles.paddingY}`}>
-            <div className="w-full text-center bg-discount-gradient p-10">
+        <section className={`flex flex-wrap text-white ${styles.paddingY}`}>
+            <div id="skills" className="w-full text-center bg-discount-gradient p-10">
                 <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] mb-10 leading-[75px] w-full">My Skills</h1>
                 <SkillList />
             </div>
